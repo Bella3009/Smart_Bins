28eb5c34049cf8d7ca3c507d686649df6939d55d
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2024-04-04 13:26:07 +0200

    Update SmartBins.py
    Added the I2C LED Display

--- SmartBins.py
+++ SmartBins.py
@@ -1,15 +1,28 @@
+import smbus
+from LCD1602 import CharLCD1602
 from gpiozero import MotionSensor
 from time import sleep
 
 # Setting GPIOs to each sensor
 pirSensor = MotionSensor(18)
+lcd1602 = CharLCD1602()
+
+def LCDDisplay(Msg1,Msg2=""):
+    lcd1602.init_lcd()
+    
+    lcd1602.clear()
+    lcd1602.write(0,0,Msg1)
+    lcd1602.write(0,1,Msg2)
+    
 
 def noMotion():
     print("No motion detected")
     sleep(1)
     
+    
 def motionDetected():
     print("Motion detected")
+    LCDDisplay("Welcome","Press button")
     sleep(1)
     
 def PIRLoop():
@@ -25,4 +38,7 @@ if __name__ == '__main__':
     try:
         PIRLoop()
     except KeyboardInterrupt:
-        print("Ending program")
+        lcd1602.clear()
+        LCDDisplay("Ending program")
+        sleep(5)
+        lcd1602.clear()
