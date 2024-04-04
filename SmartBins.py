ed0162f42ec9cab5ddd73a67f861d96a2ba9b595
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2024-04-04 14:07:53 +0200

    Update SmartBins.py
    Adding the servo motor

--- SmartBins.py
+++ SmartBins.py
@@ -1,11 +1,21 @@
 import smbus
 from LCD1602 import CharLCD1602
 from gpiozero import MotionSensor
+from gpiozero import AngularServo
 from time import sleep
 
 # Setting GPIOs to each sensor
 pirSensor = MotionSensor(18)
 lcd1602 = CharLCD1602()
+
+# Setting details for Servo Motor
+servoGPIO = 23
+SERVO_DELAY_SEC = 0.001 
+myCorrection = 0.0
+maxPW = (2.5+myCorrection)/1000
+minPW = (0.5-myCorrection)/1000
+servo = AngularServo(servoGPIO,initial_angle=0,min_angle=0, max_angle=180,min_pulse_width=minPW,max_pulse_width=maxPW)
+
 
 def LCDDisplay(Msg1,Msg2=""):
     lcd1602.init_lcd()
@@ -13,8 +23,19 @@ def LCDDisplay(Msg1,Msg2=""):
     lcd1602.clear()
     lcd1602.write(0,0,Msg1)
     lcd1602.write(0,1,Msg2)
-    
 
+def servoLoopOpen():
+    for angle in range(0, 181, 1):   # make servo rotate from 0 to 180 deg
+        servo.angle = angle
+        sleep(SERVO_DELAY_SEC)
+    sleep(0.5)
+
+def servoLoopClose():
+    for angle in range(180, -1, -1): # make servo rotate from 180 to 0 deg
+        servo.angle = angle
+        sleep(SERVO_DELAY_SEC)
+    sleep(0.5)
+        
 def noMotion():
     print("No motion detected")
     sleep(1)
@@ -23,7 +44,10 @@ def noMotion():
 def motionDetected():
     print("Motion detected")
     LCDDisplay("Welcome","Press button")
-    sleep(1)
+    sleep(3)
+    LCDDisplay("Bins No","is opening")
+    servoLoopOpen()
+    lcd1602.clear()
     
 def PIRLoop():
     while True:
@@ -39,6 +63,7 @@ if __name__ == '__main__':
         PIRLoop()
     except KeyboardInterrupt:
         lcd1602.clear()
+        servoLoopClose()
         LCDDisplay("Ending program")
         sleep(5)
         lcd1602.clear()
