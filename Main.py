abe4630a83e862d6f48f6069c1af624bba6dd54d
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2024-04-18 12:25:54 +0200

    Update Main.py
    Add the I2C display

--- Main.py
+++ Main.py
@@ -1,11 +1,14 @@
+import smbus
 from time import sleep
 import cv2
 from gpiozero import MotionSensor
+from LCD1602 import CharLCD1602
 
 imgPath = "/home/bellagauci/Documents/SmartBin/Image/"
 
 camera = cv2.VideoCapture(0)
 pirSensor = MotionSensor(10)
+lcd1602 = CharLCD1602()
 
 def captureImage():
     print("Capturing image...")
@@ -21,6 +24,12 @@ def captureImage():
         print("Failed to capture image.")
         return False
 
+def LCDDisplay(Msg1,Msg2=""):
+    lcd1602.init_lcd()
+    
+    lcd1602.clear()
+    lcd1602.write(0,0,Msg1)
+    lcd1602.write(0,1,Msg2)
 
 def noPIRMotion():
     print("No motion detected")
@@ -28,7 +37,12 @@ def noPIRMotion():
     
 def motionPIRDetected():
     print("Motion detected")
+    LCDDisplay("Welcome","Show Image")
+    sleep(2)
+    
     captureImage()
+    sleep(1.5)
+    LCDDisplay("Item Detected")
     
     
 def PIRLoop():
@@ -45,3 +59,5 @@ if __name__ == '__main__':
         PIRLoop()
     except KeyboardInterrupt:
         print("Ending program")
+        LCDDisplay("Ending program")
+        lcd1602.clear()
