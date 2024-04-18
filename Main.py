3bacb3805c4ef6a78996c4c9af2225c17ca341c0
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2024-04-18 11:38:09 +0200

    Update Main.py
    Start to seperate the components in different files. This code will signal the start.

--- Main.py
+++ Main.py
@@ -1,1 +1,26 @@
+from time import sleep
+from gpiozero import MotionSensor
 
+pirSensor = MotionSensor(10)
+
+def noPIRMotion():
+    print("No motion detected")
+    sleep(1)
+    
+def motionPIRDetected():
+    print("Motion detected")
+    sleep(1)
+    
+def PIRLoop():
+    while True:
+        if pirSensor.value == 1:
+            motionPIRDetected()
+        else:
+            noPIRMotion()
+
+if __name__ == '__main__':
+    print('Program is starting ... ')
+    try:
+        PIRLoop()
+    except KeyboardInterrupt:
+        print("Ending program")
