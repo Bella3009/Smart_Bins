0cea6f4d77089094f11fd3f54b6546a4f62f482e
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2024-04-20 23:22:15 +0200

    Update LCDDisplay.py
    Add the speech output when Showing message. Function info still needs to be perfected.

--- LCDDisplay.py
+++ LCDDisplay.py
@@ -1,20 +1,35 @@
 import smbus
 from LCD1602 import CharLCD1602
+from pygame import mixer
+
+audioPath = "/home/bellagauci/Documents/SmartBin/Audio/"
 
 lcd1602 = CharLCD1602()
+mixer.init() # Speaker setting
 
-def DisplayMsg(Msg1,Msg2=""):
+def info(audioName):
+    mixer.music.stop()
+    mixer.music.set_volume(1.0)
+    mixer.music.load(audioPath+audioName+".mp3")
+    mixer.music.play()
+    while mixer.music.get_busy() == True:
+        continue
+    
+def displayMsg(audio,Msg1,Msg2=""):
     lcd1602.init_lcd()
     
     lcd1602.clear()
     lcd1602.write(0,0,Msg1)
     lcd1602.write(0,1,Msg2)
     
+    info(audio)
+    
+    
 if __name__ == '__main__':
     print('Program is starting ... ')
     try:
-        DisplayMsg("Welcome")
+        displayMsg("Welcome", "Welcome")
     except KeyboardInterrupt:
         print("Ending program")
-        DisplayMsg("Ending program")
+        displayMsg("Ending program")
         lcd1602.clear()
