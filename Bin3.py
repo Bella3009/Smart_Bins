158cda91ea090e638cc213d52ef87d0a3011b7e1
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2024-04-21 22:17:43 +0200

    Create Bin3.py
    Initial commit for Bin3

---
+++ Bin3.py
@@ -0,0 +1,83 @@
+from time import sleep
+from gpiozero import AngularServo
+from LCDDisplay import displayMsg
+from gpiozero import DigitalInputDevice
+from gpiozero import DistanceSensor
+from gpiozero import RGBLED
+
+binNo = "3"
+
+irSensor = DigitalInputDevice(11)
+
+# Setting details for Servo Motor
+servoGPIO = 18
+SERVO_DELAY_SEC = 0.001
+myCorrection = 0.0
+maxPW = (2.5+myCorrection)/1000
+minPW = (0.5-myCorrection)/1000
+servo = AngularServo(servoGPIO,initial_angle=0,min_angle=0, max_angle=180,min_pulse_width=minPW,max_pulse_width=maxPW)
+
+# Setting details for Distance Sensor
+trigPin = 5
+echoPin = 21
+sensor = DistanceSensor(echo=echoPin, trigger=trigPin ,max_distance=3)
+binDepth = 20
+halfEmpty = 23/2
+nearFull = 23/4 # /4 because 4 = 25% meaning that only 25% of the bin is empty
+
+# Setting details for RGB LED
+rLED = 26
+gLED = 19
+bLED = 13
+led = RGBLED(red=rLED, green=gLED, blue=bLED, active_high=True)
+
+def servoOpen():
+    # For this servo the negative angle makes the most confortable way to open the bin
+    displayMsg("Bin" + binNo + "Open","Bins "+ binNo,"is opening")
+    for angle in range(180, -1, -1):   # make servo rotate from 0 to 180 deg
+        servo.angle = angle
+        sleep(SERVO_DELAY_SEC)
+    sleep(0.5)
+
+def servoClose(endProg=False):
+    if endProg == False: # With this condition, the message will not appear when the program ends
+        displayMsg("Bin" + binNo + "Close","Bins "+ binNo,"is closing")
+    for angle in range(0, 181, 1): # make servo rotate from 180 to 0 deg
+        servo.angle = angle
+        sleep(SERVO_DELAY_SEC)
+    sleep(0.5)
+
+def motionIRDetected():
+    print("Item thrown")
+    sleep(1)
+    
+def noIRMotion():
+    print("Item not yet thrown")
+    sleep(1)
+
+def setColor(rDepth):
+    if rDepth <= nearFull:
+        led.color = (1,0,0)
+        displayMsg("PressButton","Bin " + binNo +" is", "nearly full") # Audio needs to be changed once audio file is done
+        sleep(2)
+    
+    elif rDepth <= halfEmpty and rDepth > nearFull:
+        led.color = (1,1,0)
+        displayMsg("PressButton","Bin " + binNo +" is","half full") # Audio needs to be changed once audio file is done
+        sleep(2)
+    
+    elif rDepth > halfEmpty:
+        led.color = (0,1,0)
+
+def distanceMeasure():
+    distance = sensor.distance * 100
+    distance = round(distance, 2)
+    print(distance)
+    setColor(distance)
+    
+if __name__ == "__main__":
+    servoOpen()
+    distanceMeasure()
+    sleep(2)
+    servoClose()
+    led.color = (0, 0, 0)  # off
