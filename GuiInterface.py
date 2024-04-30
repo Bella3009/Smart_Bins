853f67fb078f48e256eb6a8b2d08946cbe19a034
Bella3009 <bellasara13@gmail.com>
2024-04-30 23:11:35 +0200

    Added the title on top of the window

--- GuiInterface.py
+++ GuiInterface.py
@@ -1,6 +1,12 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
-window.title("Smart Bins Dashboard")
+window = Tk() 
+window.title("Smart Bins Dashboard") # Set the title of the window
+
+# Set the widgets
+title = Label(window, text="Smart Bins")
+
+# Place them in the window using the grid method
+title.grid(row=0,column=0,columnspan=3)
 
 window.mainloop()
