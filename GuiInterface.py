f7aec1ac58e24d6cd1bdbd1172aa2d11a4888501
Bella3009 <bellasara13@gmail.com>
2024-05-01 15:36:25 +0200

    Set the width of the titles and added the second row label

--- GuiInterface.py
+++ GuiInterface.py
@@ -5,14 +5,20 @@ window.title("Smart Bins Dashboard") # Set the title of the window
 
 # Set the widgets
 title = Label(window, text="Smart Bins")
-bin1Title = Label(window, text="Organic")
-bin2Title = Label(window, text="Mixed")
-bin3Title = Label(window, text="Recycle")
+bin1Title = Label(window, text="Organic", width=25)
+bin1Label = Label(window, text="Bin 1", width=25)
+bin2Title = Label(window, text="Mixed", width=25)
+bin2Label = Label(window, text="Bin2", width=25)
+bin3Title = Label(window, text="Recycle", width=25)
+bin3Label = Label(window, text="Bin 3", width=25)
 
 # Place them in the window using the grid method
 title.grid(row=0,column=0,columnspan=3)
 bin1Title.grid(row=1,column=0)
 bin2Title.grid(row=1,column=1)
 bin3Title.grid(row=1,column=2)
+bin1Label.grid(row=2,column=0)
+bin2Label.grid(row=2,column=1)
+bin3Label.grid(row=2,column=2)
 
 window.mainloop()
