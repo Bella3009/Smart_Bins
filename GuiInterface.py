ac2ee8695451ef680d9bf39ea5f4cb7b5e6d75fe
Bella3009 <bellasara13@gmail.com>
2024-05-01 22:40:58 +0200

    Function to calculate the empty percentage

--- GuiInterface.py
+++ GuiInterface.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
+binDepth = 23
+
 def binStatus(value):
-    binDepth = 23
     halfEmpty = binDepth/2 # 11.5
     nearFull = binDepth/4 # 5.75
     status = "Image/"
@@ -18,6 +19,13 @@ def binStatus(value):
 
     return status
 
+def calcPercentage(number):
+    number = float(number)
+    value = number/binDepth
+    percent = value * 100
+    percent = round(percent,2)
+    print(percent)
+
 with open("Data/Measure1.txt","r") as file:
     reading1 = file.read()
 
@@ -27,9 +35,10 @@ with open("Data/Measure2.txt","r") as file:
 with open("Data/Measure3.txt","r") as file:
     reading3 = file.read()
 
-print(float(reading1))
-print(float(reading2))
-print(float(reading3))
+calcPercentage(reading1)
+calcPercentage(reading2)
+calcPercentage(reading3)
+
 bin1Status = binStatus(float(reading1)) 
 bin2Status = binStatus(float(reading2)) 
 bin3Status = binStatus(float(reading3))
@@ -48,6 +57,8 @@ bin2Title = Label(window, text="Mixed", width=25)
 bin2Label = Label(window, text="Bin2", width=25)
 bin3Title = Label(window, text="Recycle", width=25)
 bin3Label = Label(window, text="Bin 3", width=25)
+
+# Image selection and display 
 img1 = ImageTk.PhotoImage(Image.open(bin1Status))
 img2 = ImageTk.PhotoImage(Image.open(bin2Status))
 img3 = ImageTk.PhotoImage(Image.open(bin3Status))
